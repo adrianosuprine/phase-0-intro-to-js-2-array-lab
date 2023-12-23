a0e82563104b364839259a195c2a897df46871eb
adrianosuprine <adrianosuprine@gmail.com>
2023-12-23 21:29:44 +0300

    Add destructive and nondestructive arrays

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = [];
+cats.push("Milo", "Otis", "Garfield");
+function destructivelyAppendCat(name){
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+  cats.unshift("Bob");
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift("Milo");
+}
+function destructivelyRemoveLastCat(){
+  cats.pop("Garfield");
+}
+function appendCat(name){
+  const copyOfCats = cats.slice();
+  copyOfCats.push("Broom"); 
+  return copyOfCats;
+}
+function prependCat(name){
+  const copyOfCatsTwo = cats.slice();
+  copyOfCatsTwo.unshift("Arnold"); 
+  return copyOfCatsTwo;
+}
+function removeFirstCat(){
+   const removeFirstCat=cats.slice(1);
+   return removeFirstCat;
+}
+function removeLastCat(){
+   const removeLastCat=cats.slice(0,-1);
+   return removeLastCat;  
+}
+
